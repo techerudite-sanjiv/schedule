2d6f31c9f86e05b0d2c4e4b6db5162945ce00190
Sanjeev Kumar <sanjiv@ttecherudite.com>
2023-03-01 18:51:44 +0530

    add code

--- src/eventData.js
+++ src/eventData.js
@@ -7,16 +7,18 @@ export const bookings = [
   },
   {
     name: "Event2",
-    eventStartTime: "12:00",
-    eventEndTime: "13:00",
-    treatment:"Body Care"
-  }, {
-    name: "Event2",
     eventStartTime: "14:00",
     eventEndTime: "14:50",
     treatment:"Botox 30ml"
-  }, {
-    name: "Event2",
+  }, 
+  {
+    name: "Event3",
+    eventStartTime: "12:00",
+    eventEndTime: "13:00",
+    treatment:"Body Care"
+  }, 
+  {
+    name: "Event5",
     eventStartTime: "15:00",
     eventEndTime: "18:00",
     treatment:"Treatment"
